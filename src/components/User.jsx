9c641c9a9f6be03486e224618b234f506f95da74
Irnika <redduna@ukr.net>
2020-03-15 16:45:55 +0200

    Add project

---
+++ src/components/User.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function User(props) {
+    return <li> firstName: {props.user.firstName}, lastName: {props.user.lastName}, age: {props.user.age}, job: {props.user.job}</li>;
+}
+
+function UserList() {
+    const users = [
+        { id: 1, firstName: "John", lastName: "Doe", age: 24, job: "Developer"},
+        { id: 2, firstName: "Kate", lastName: "Reton", age: 22, job: "Chef"},
+        { id: 3, firstName: "Melissa", lastName: "Lout", age: 41, job: "Developer"},
+        { id: 4, firstName: "Ann", lastName: "Willington", age: 23, job: "Driver"},
+        { id: 5, firstName: "Michel", lastName: "Plank", age: 55, job: "Developer"},
+        { id: 6, firstName: "Bob", lastName: "Palanski", age: 32, job: "Waiter"},
+        { id: 7, firstName: "Jack", lastName: "Daniels", age: 23, job: "Developer"},
+        { id: 8, firstName: "Poul", lastName: "Torn", age: 65, job: "Cashier"},
+    ];
+    return (
+        <ol>
+            {users.map((user) => <User key={user.id} user={user} />)}
+        </ol>
+    );
+}
+
+export default UserList;
---
+++ src/components/Video.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Video = ({url, name, isVideo, click, number, text, children}) => (
+    <div >
+       
+        
+    </div>
+);
+
+export default Video;
